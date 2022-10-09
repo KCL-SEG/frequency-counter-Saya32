e03e3d4ffae00c68910df25f6036cc58426513b9
Sayaka Bhandari <sayakabhandari@gmail.com>
2022-10-09 17:08:55 +0100

    final change

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,12 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    #traverse over the items
+    for i in items: 
+        i=str(i)
+        if i in frequencies.keys(): #increment the value if it's present in dictionary
+            frequencies[i]+=1
+        else:
+            frequencies[i]=1 #otherwise, assign the key
     return frequencies
